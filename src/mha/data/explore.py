ecc94213b9e9a7438ab9aabe500c4f4f1683e85f
Vikram Kumar <vkverma9534@gmail.com>
2026-05-13 15:52:05 +0530

    Added tentative logic for ticker search

---
+++ src/mha/data/explore.py
@@ -0,0 +1,21 @@
+import requests
+
+def TickerSearch(name: str):
+      """
+      Takes an entity or firm name as input and returns possible matching
+      organizations with their ticker symbols.
+      """
+    r = requests.get(
+        "https://query1.finance.yahoo.com/v1/finance/search",
+        params={"q": name, "quotesCount": 10, "newsCount": 0},
+        headers={"User-Agent": "Mozilla/5.0"}
+    ).json()
+
+    return [
+        {
+            "name": x.get("shortname"),
+            "ticker": x.get("symbol")
+        }
+        for x in r.get("quotes", [])
+        if x.get("symbol")
+    ]
