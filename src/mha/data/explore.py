efb8d3c6bc96a71ba01d15699c1755f3809326d7
Vikram Kumar <vkverma9534@gmail.com>
2026-05-14 21:42:37 +0530

    Adding corrected version for Tickersearch

--- src/mha/data/explore.py
+++ src/mha/data/explore.py
@@ -1,10 +1,11 @@
 import requests
 
 def TickerSearch(name: str):
-      """
-      Takes an entity or firm name as input and returns possible matching
-      organizations with their ticker symbols.
-      """
+    """
+    Takes an entity or firm name as input and returns possible matching
+    organizations with their ticker symbols.
+    """
+
     r = requests.get(
         "https://query1.finance.yahoo.com/v1/finance/search",
         params={"q": name, "quotesCount": 10, "newsCount": 0},
